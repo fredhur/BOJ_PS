21239fba69441dd489854786fb0c8e309a8efb4a
fredhur <fredhur36@gmail.com>
2022-06-26 23:43:52 +0900

    Solve 24480

---
+++ BOJ_2xxxx/boj_24480.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<vector<int>> v(100005);
+int N, M, R;
+bool visited[100005];
+int visitCnt[100005];
+int cnt;
+void dfs(int idx)
+{
+    visitCnt[idx] = ++cnt;   
+    for(auto &i: v[idx])
+    {
+        if(visited[i] == true) continue;
+       
+        visited[i] = true;
+        dfs(i);
+    }
+}
+
+int main()
+{
+    
+    scanf("%d %d %d", &N, &M, &R);
+
+    for(int i=1; i<=M; i++)
+    {
+        int ind, n;
+        scanf(" %d %d", &ind, &n);
+        v[ind].push_back(n);
+        v[n].push_back((ind));
+    }
+
+    for(int i=1; i<=N; i++)
+    {
+       sort(v[i].begin(),v[i].end(), greater<int>());
+    }
+    visited[R] = true;
+    dfs(R);
+
+    for(int i=1; i<=N; i++)
+    {
+        printf("%d\n", visitCnt[i]);
+    }
+}
+
