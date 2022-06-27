e0b8f57e235341abb78c04e0808ecaeadd01d79b
fredhur <fredhur36@gmail.com>
2022-06-28 01:34:53 +0900

    Solve 25306

---
+++ BOJ_2xxxx/boj_25306.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using ll = long long;
+
+ll xorSum(ll n)
+{
+    const ll lut[] = {
+        n,
+        1,
+        n + 1,
+        0};
+    const auto m = n & (4-1);
+    return lut[m];
+}
+ll A, B;
+int main()
+{
+    std::cin >> A >> B;
+    const ll ret = xorSum(A - 1) ^ xorSum(B);
+    std::cout << ret;
+    return 0;
+}
