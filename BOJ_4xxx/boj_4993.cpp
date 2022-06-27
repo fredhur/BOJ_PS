afb27358c82d300ff072b8d3b7ed0e58e61de810
fredhur <fredhur36@gmail.com>
2022-06-28 00:49:02 +0900

    Solve 4993 as class

--- BOJ_4xxx/boj_4993.cpp
+++ BOJ_4xxx/boj_4993.cpp
@@ -1,64 +1,83 @@
 #include <iostream>
 using namespace std;
-char map[30][30];
-int W, H;
-int dirx[] = {1, -1, 0, 0};
-int diry[] = {0, 0, 1, -1};
-int initY, initX;
-bool visited[30][30];
-int ans;
-void init()
-{
-    ans =0;
-    for(int i=0; i<30; i++)
-    {
-        for(int j=0; j<30;j++)
-        {
-            visited[i][j] = 0;
-        }
-    }
-}
-void input()
-{
 
-    for (int i = 0; i < H; i++)
+class Solve
+{
+public:
+    Solve() = default;
+    ~Solve() = default;
+    bool InitInput()
     {
-        for (int j = 0; j < W; j++)
+        scanf("%d %d", &W, &H);
+        if (W == 0 && H == 0) return false;
+            
+        for (int i = 0; i < H; i++)
         {
-            scanf(" %1c", &map[i][j]);
-            if(map[i][j] == '@')
+            for (int j = 0; j < W; j++)
             {
-                initY = i, initX = j;
+                scanf(" %1c", &map[i][j]);
+                if (map[i][j] == '@')
+                {
+                    initY = i, initX = j;
+                }
+            }
+        }
+        return true;
+    }
+    void init()
+    {
+        ans = 0;
+        for (int i = 0; i < 30; i++)
+        {
+            for (int j = 0; j < 30; j++)
+            {
+                visited[i][j] = 0;
             }
         }
     }
-}
-void dfs(int cy, int cx)
-{
 
-    for(int i=0; i<4; i++)
+    void Dfs()
     {
-        int ny = cy + diry[i];
-        int nx = cx + dirx[i];
+        dfs(initY, initX);
+    }
+    void dfs(int cy, int cx)
+    {
 
-        if(0<=ny && ny < H && 0<= nx && nx < W && visited[ny][nx] ==false && map[ny][nx] != '#' && map[ny][nx] == '.' )
+        for (int i = 0; i < 4; i++)
         {
-            visited[ny][nx] = true;
-            ans++;
-            dfs(ny,nx);
+            int ny = cy + diry[i];
+            int nx = cx + dirx[i];
+
+            if (0 <= ny && ny < H && 0 <= nx && nx < W && visited[ny][nx] == false && map[ny][nx] != '#' && map[ny][nx] == '.')
+            {
+                visited[ny][nx] = true;
+                ans++;
+                dfs(ny, nx);
+            }
         }
     }
-}
+    int GetAns() const 
+    {
+        return ans;
+    }
+private:
+    int W, H, ans;
+    int dirx[4] = {1, -1, 0, 0};
+    int diry[4] = {0, 0, 1, -1};
+    int initY, initX;
+    char map[30][30];
+    bool visited[30][30];
+}solve;
+
 int main()
 {
 
-    while (true)
+    while (solve.InitInput())
     {
-        scanf("%d %d", &W, &H);
-        if (W == 0 && H == 0) break;
-        input();
-        init();
-        dfs(initY, initX);
-        printf("%d\n", ans + 1);
+      
+        solve.init();
+        solve.Dfs();
+       
+        printf("%d\n", solve.GetAns()+1);
     }
 }
