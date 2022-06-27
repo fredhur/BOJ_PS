1a4ae0d6af140bd099b115c388e92854e72e1a51
fredhur <fredhur36@gmail.com>
2022-06-28 01:04:16 +0900

    Solve 25305

---
+++ BOJ_2xxxx/boj_25305.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> members;
+
+int N,K;
+int main()
+{
+    scanf("%d %d", &N, &K);
+    for(int i=0; i<N; i++)
+    {
+        int m;
+        scanf(" %d", &m);
+        members.push_back(m);
+    }
+    std::sort(members.begin(), members.end(),greater<int>());
+    printf("%d", members[K-1]);
+}
