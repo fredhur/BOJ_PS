74792bda67dbfa5c966bcb27353d0ed9dc32900a
fredhur <fredhur36@gmail.com>
2022-07-02 17:20:56 +0900

    Solve 4889

---
+++ BOJ_4xxx/boj_4889.cpp
@@ -0,0 +1,60 @@
+#include <cstdio>
+#include <stack>
+#include <string>
+using namespace std;
+int ans;
+
+char str[2500];
+auto strLen(char (&str)[2500])
+{
+    auto i = 0;
+    for (i = 0; str[i] != '\0'; i++)
+        ;
+    return i;
+}
+int main()
+{
+    int idx = 0;
+    while (true)
+    {
+        scanf("%s", str);
+        getchar();
+        if (str[0] == '-')
+            break;
+
+        stack<char> stk;
+        ans = 0;
+       
+
+        for (auto i = 0; i < strLen(str); i++)
+        {
+
+            if (str[i] == '{')
+            {
+                stk.push('{');
+            }
+            else
+            {
+                if (!stk.empty())
+                {
+                    stk.pop();
+                }
+                else
+                {
+                    ans++;
+                    stk.push('{');
+                }
+            }
+        }
+        if (stk.empty())
+        {
+            printf("%d. %d\n", ++idx, ans);
+        }
+        else
+        {
+            printf("%d. %d\n", ++idx, ans + stk.size() / 2);
+        }
+    }
+
+    return 0;
+}
