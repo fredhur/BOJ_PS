df55f636a1bf21e5043eb3da60bc799fd70d0199
fredhur <fredhur36@gmail.com>
2022-07-02 17:56:40 +0900

    Solve 4375

---
+++ BOJ_4xxx/boj_4375.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+using ll = long long;
+ll m;
+int main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    m = 1;
+    while (cin >> n)
+    {
+        int cnt = 1;
+        while (m % n != 0)
+        {
+            m = m % n;
+            m = m * 10 + 1;
+            cnt++;
+        }
+        cout << cnt << '\n';
+        m = 1;
+    }
+}
