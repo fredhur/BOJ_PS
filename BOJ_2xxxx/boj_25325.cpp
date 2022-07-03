10a23e77fde492fd727f126bcfd915d792d3ff03
fredhur <fredhur36@gmail.com>
2022-07-03 21:16:09 +0900

    Solve 25325 (python)

---
+++ BOJ_2xxxx/boj_25325.cpp
@@ -0,0 +1,16 @@
+
+// using python to solve 25325
+
+
+
+// s = dict()
+// n = int(input())
+// sl = input().split()
+// for i in sl: s[i] = 0
+// for i in range(0,n):
+//     t = input().split()
+//     for j in t : s[j]+=1
+// s = dict(sorted(s.items(), key = lambda x :x[0].lower()))
+// r = dict(sorted(s.items(), key = lambda x:(x[1]), reverse=True))
+// for k,v in r.items():
+//     print("{0} {1}".format(k,v))
