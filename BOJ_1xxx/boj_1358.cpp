ebd03203a8bb34e05691ab7f2fe3d9c25d4e5801
fredhur <fredhur36@gmail.com>
2022-07-03 21:13:33 +0900

    Solve 1358

---
+++ BOJ_1xxx/boj_1358.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+// think it as X,Y axis.
+int W, H, X, Y, P, radius;
+int ans;
+int main()
+{
+    scanf("%d%d%d%d%d", &W, &H, &X, &Y, &P);
+    radius = H/2;
+    for(auto i=0; i<P; i++)
+    {
+        int x,y;
+        scanf(" %d%d", &x, &y);
+        if(0<=x-X && x-X <=W && 0<=y-Y && y-Y <= H)
+        {
+            ans++;
+            continue;
+        }
+        else 
+        {
+            auto pX = x-X;
+            auto pY = y-Y -radius;
+            if(pX <=0 && pX*pX + pY*pY <= radius*radius)
+            {
+                ans++;
+                continue;
+            }
+            pX -=W;
+            if(pX >=0 && pX*pX + pY*pY <= radius*radius)
+            {
+                ans++;
+                continue;
+            }
+        }
+    }
+    printf("%d", ans);
+}
