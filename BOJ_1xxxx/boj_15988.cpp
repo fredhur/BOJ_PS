89bd36213fb56e0bb8f6e1a69c60be23b20a1b40
fredhur <fredhur36@gmail.com>
2022-07-08 23:52:13 +0900

    Solve 15988

---
+++ BOJ_1xxxx/boj_15988.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+using ll = long long;
+static constexpr auto SIZE = 1'000'007;
+static constexpr auto mod = 1'000'000'009;
+
+static ll arr[SIZE];
+constexpr static void init()
+{
+    
+    arr[1] = 1, arr[2] = 2, arr[3] = 4, arr[4] = 7;
+
+    for (auto i = 4; i < SIZE; i++)
+    {
+        arr[i] = (arr[i - 1] % mod + arr[i - 2] % mod + arr[i - 3] % mod) % mod;
+    }
+}
+int main()
+{
+    init();
+    int tc;
+    scanf("%d", &tc);
+    while (tc--)
+    {
+        int n;
+        scanf(" %d", &n);
+        printf("%lld\n", arr[n]);
+    }
+    return 0;
+}
