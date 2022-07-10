17fe9e45324c7fc5d21ca636868513b0af026893
fredhur <fredhur36@gmail.com>
2022-07-10 23:25:04 +0900

    Solve 17945

---
+++ BOJ_1xxxx/boj_17945.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+class Solve
+{
+public:
+    void solve()
+    {
+        scanf("%d %d", &A, &B);
+        auto D = sqrt(A * A - B);
+        if (D == 0) printf("%d", -A);
+        else printf("%d %d", -D - A, D - A);
+    }
+private:
+    int sqrt(int n)
+    {
+        int s = 1, e = n;
+        int ans = n;
+        while (s <= e)
+        {
+            int m = (s + e) / 2;
+            if (m <= n / m)
+                s = m + 1, ans = m;
+            else
+                e = m - 1;
+        }
+        return ans;
+    }int A,B;
+}s;
+int main()
+{
+    s.solve();
+}
