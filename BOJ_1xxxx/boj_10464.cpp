783d3bb2cfd5ed61307a46a6ebba7539e3bea942
fredhur <fredhur36@gmail.com>
2022-07-17 23:11:35 +0900

    Solve 10464

---
+++ BOJ_1xxxx/boj_10464.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using ll = long long;
+ll xorAtoB(ll n)
+{
+    const ll lut[]=
+    {
+        n,
+        1,
+        n+1,
+        0
+    };
+    const auto m = n&(4-1);
+    return lut[m];
+}
+int main()
+{
+    int tc;
+    scanf("%d", &tc);
+    while(tc--)
+    {
+        ll s, f;
+        scanf(" %lld %lld", &s, &f);
+        printf("%lld\n", xorAtoB(s-1)^xorAtoB(f));
+    }
+    return 0;
+}
