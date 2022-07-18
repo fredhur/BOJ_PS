7dc390277c07704cd2d95ea6487843d58574c2bb
fredhur <fredhur36@gmail.com>
2022-07-18 21:15:10 +0900

    Solve 10865

---
+++ BOJ_1xxxx/boj_10865.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int N,M;
+vector<vector<int>> v(100'007);
+int main()
+{
+    scanf("%d %d", &N, &M);
+    while(M--)
+    {
+        int a, b;
+        scanf(" %d %d", &a, &b);
+        v[a].push_back(b);
+        v[b].push_back(a);
+    }
+
+    for(int i=1; i<=N; i++)
+    {
+        printf("%d\n", v[i].size());
+    }
+}
