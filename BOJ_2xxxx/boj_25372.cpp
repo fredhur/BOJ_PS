0175814edac1daab2fd79aa94ad8626be48e8b78
fredhur <fredhur36@gmail.com>
2022-07-22 20:53:31 +0900

    Sovle 25372

---
+++ BOJ_2xxxx/boj_25372.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+const char *Calc(char *pwd)
+{
+
+    auto isOkay = [](const int &len)
+    { return len >= 6 && len <= 9; };
+    auto mstrlen = [](char *s)
+    {  int i = 0;
+        for (i = 0; s[i]; i++);
+        return i; };
+    return isOkay(mstrlen(pwd)) ? "yes" : "no";
+}
+int main()
+{
+    int N;
+    char str[22];
+    scanf("%d", &N);
+    while (N--)
+    {
+        scanf(" %s", str);
+        printf("%s\n", Calc(str));
+    }
+}
