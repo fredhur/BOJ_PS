a330f37092e8fc3768a4a8412bc5759cb9d84b5f
fredhur <fredhur36@gmail.com>
2022-07-22 20:40:34 +0900

    Solve 2858

---
+++ BOJ_2xxx/boj_2858.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+int R, B;
+int main()
+{
+    scanf("%d %d", &R, &B);
+    const auto tot = R + B;
+    for (int i = tot; i >= 1; i--)
+    {
+        const auto t = tot / i;
+        if (tot % i == 0 && ((t + t + i + i - 4) == R))
+        {
+            printf("%d %d", i, t);
+            return 0;
+        }
+    }
+    return 0;
+}
