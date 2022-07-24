2c51e56070f2ad38ac9a4e837e52e4b4c554acd5
fredhur <fredhur36@gmail.com>
2022-07-24 22:38:05 +0900

    Solve 4158

---
+++ BOJ_4xxx/boj_4158.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+using uint = unsigned int;
+uint cd[1000007];
+int main()
+{
+    while(true)
+    {
+        uint N, M;
+        scanf("%u %u", &N, &M);
+        if(N==0 && M==0) break;
+        uint ans = 0;
+        for(int i=0; i<N; i++)
+        {
+            scanf(" %u", &cd[i]);
+        }
+        while(M--)
+        {
+            uint tar;
+            scanf(" %u", &tar);
+            ans += binary_search(cd, cd+N, tar);
+        }
+        printf("%u\n", ans);
+    }
+    return 0;
+}
