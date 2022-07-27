6646e5a2aa7a6eab5cbc6d8e0372fcb30edafa6f
fredhur <fredhur36@gmail.com>
2022-07-27 14:20:52 +0900

    Solve 14175

---
+++ BOJ_1xxxx/boj_14175.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int M, N, K;
+    scanf("%d %d %d", &M, &N, &K);
+    for(int i=0; i<M; i++)
+    {
+        string str;
+        for(int j=0; j<N; j++)
+        {
+            char ch;
+            scanf(" %1c", &ch);
+            for(int k=0; k<K; k++) str+= ch;
+        }
+        for(int k=0; k<K; k++) cout << str << '\n';
+    }
+}
