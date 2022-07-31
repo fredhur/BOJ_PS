5efc6f3c4185ce0ab0b2ecd9c933e52e10dff2e6
fredhur <fredhur36@gmail.com>
2022-07-31 23:02:57 +0900

    Solve 25377

---
+++ BOJ_2xxxx/boj_25377.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+int N;
+#define INT_MAX (0x7fffffff)
+template <typename T>
+inline T min(T a, T b)
+{
+    return a < b ? a : b;
+}
+int main()
+{
+    scanf("%d", &N);
+    int ans = INT_MAX;
+    while(N--)
+    {
+        int A, B;
+        scanf(" %d %d", &A, &B);
+        if(A<=B)
+        {
+            ans = min(ans, B);
+        }
+    }   
+    printf("%d", ans == INT_MAX ? -1 : ans);
+    return 0;
+}
