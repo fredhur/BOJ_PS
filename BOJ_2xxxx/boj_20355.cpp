5badcfd355a744de5bd6c83917c6a9218b141543
fredhur <fredhur36@gmail.com>
2022-08-03 00:39:37 +0900

    Solve 20355

---
+++ BOJ_2xxxx/boj_20355.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+int CalcCaesarStr(char *str)
+{
+    int cnt = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        int icnt = 0;
+        for (int j = 0; str[j]; j++)
+        {
+            const auto ch = ((str[j]-'a' + i) % 26) + 'a';
+            icnt += (ch=='i');
+        }
+        cnt += (icnt == 0);
+       
+    }
+    return cnt;
+}
+int main()
+{
+    char str[105];
+    scanf("%s", str);
+    const auto ret = CalcCaesarStr(str);
+    if(ret ==0)
+    {
+        puts("impossible");
+    }
+    else 
+    {
+        printf("%d",ret);
+    }
+    return 0;
+}
