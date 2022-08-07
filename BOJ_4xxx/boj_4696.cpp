33a360146b683263430f7459ac6c3c9c3041bf08
fredhur <fredhur36@gmail.com>
2022-08-08 00:17:53 +0900

    Solve 4696

---
+++ BOJ_4xxx/boj_4696.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+int main()
+{
+    while(true)
+    {
+        double N;
+        scanf("%lf", &N);
+        if(N==0) break;
+        double ans = 0;
+        for(int i=0; i<5; i++)
+        {
+            ans = ans*N + 1;
+        }
+        printf("%.2lf\n", ans);
+    }
+}
