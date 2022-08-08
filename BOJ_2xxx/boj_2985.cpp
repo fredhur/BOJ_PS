70d8a59d12c6e7cc05286cd6f9d1219f3d8f48b6
fredhur <fredhur36@gmail.com>
2022-08-09 00:26:28 +0900

    Solve 2985

---
+++ BOJ_2xxx/boj_2985.cpp
@@ -0,0 +1,95 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    scanf("%d%d%d", &a, &b, &c);
+    char op[4] = {'+', '-', '*', '/'};
+    char ret[2] = {
+        0,
+    };
+    for (int i = 0; i < 4; i++)
+    {
+        switch (op[i])
+        {
+        case '+':
+            if (a == (b + c))
+            {
+                ret[0] = '=';
+                ret[1] = '+';
+                // printf("here");
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+            else if ((a + b) == c)
+            {
+                ret[0] = '=';
+                ret[1] = '+';
+                swap(ret[0], ret[1]);
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+            
+            break;
+        case '-':
+
+            if (a == (b - c))
+            {
+                ret[0] = '=';
+                ret[1] = '-';
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+            else if ((a - b) == c)
+            {
+                ret[0] = '=';
+                ret[1] = '-';
+                swap(ret[0], ret[1]);
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+           
+            break;
+        case '*':
+            if (a == (b * c))
+            {
+                ret[0] = '=';
+                ret[1] = '*';
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+            else if ((a * b) == c)
+            {
+                ret[0] = '=';
+                ret[1] = '*';
+                swap(ret[0], ret[1]);
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+            
+            break;
+        case '/':
+
+            if (a == (b / c))
+            {
+                ret[0] = '=';
+                ret[1] = '/';
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+            else if ((a / b) == c)
+            {
+                ret[0] = '=';
+                ret[1] = '/';
+                swap(ret[0], ret[1]);
+                printf("%d%c%d%c%d", a, ret[0], b, ret[1], c);
+                return 0;
+            }
+            
+            break;
+        }
+    }
+    
+    return 0;
+}
