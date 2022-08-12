8868197a322a785e681cfd059271eb1a11fb837c
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-13 00:28:04 +0900

    Solve 25400

---
+++ BOJ_2xxxx/boj_25400.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+int arr[250007];
+int N;
+int main()
+{
+    int del = 0;
+    int chk = 1;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf(" %d", &arr[i]);
+    }
+
+    for(int i=0; i<N; i++)
+    {
+        if(arr[i] == chk)
+        {
+            chk++;
+            continue;
+        }
+        else 
+        {
+            del++;
+        }
+    }
+    printf("%d\n", del);
+    return 0;
+}
