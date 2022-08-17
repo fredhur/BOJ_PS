9e1bb4b620087d7887614204423201a8875a4a5f
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-18 01:06:02 +0900

    Solve 24087

---
+++ BOJ_2xxxx/boj_24087.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+int S, A, B;
+int main()
+{
+    scanf("%d %d %d", &S, &A, &B);
+    const auto diff = S - A;
+    if (diff <= 0)
+    {
+        puts("250");
+    }
+    else
+    {
+        const int tot = (diff % B == 0) ? diff / B : diff / B + 1;
+        printf("%d\n", 250 + 100 * tot);
+    }
+}
