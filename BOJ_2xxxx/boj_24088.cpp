efd3cfd8b7c369108ee2f609b83365d91c0eb86f
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-18 01:11:05 +0900

    Solve 24088

---
+++ BOJ_2xxxx/boj_24088.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+int N;
+int K;
+char str[2005];
+int main()
+{
+    scanf("%d", &N);
+    scanf(" %d", &K);
+    scanf(" %s", str);
+    int R = 0, W = 0;
+    for (int i = 0; str[i]; i++)
+    {
+        R += str[i] == 'R';
+        W += str[i] == 'W';
+    }
+    puts(R == K ? "W" : "R");
+
+    return 0;
+}
