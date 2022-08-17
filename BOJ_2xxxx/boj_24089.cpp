4d07eca531490c62bffc4265bbcba0ce53857b05
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-18 01:19:31 +0900

    Solve 24089

---
+++ BOJ_2xxxx/boj_24089.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+int N, M;
+int ballIdx[2005];
+void init()
+{
+    for(int i=1; i<=N; i++)
+    {
+        ballIdx[i] = i;
+    }
+}
+int main()
+{
+    scanf("%d %d", &N, &M);  
+    init();
+    for(int i=0; i<M; i++)
+    {
+        int X,Y;
+        scanf(" %d %d", &X, &Y);
+        ballIdx[X] = Y;
+    }
+    for(int i=1; i<=N; i++)
+    {
+        printf("%d\n", ballIdx[i]);
+    }
+    return 0;
+}
