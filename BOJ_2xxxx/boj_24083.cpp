4a8f07dc56deae94afe4af9ff1ad866542c72f7e
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-18 01:05:48 +0900

    Solve 24083

--- BOJ_2xxxx/boj_24083.cpp
+++ BOJ_2xxxx/boj_24083.cpp
@@ -1,8 +1,8 @@
 #include <cstdio>
 int main()
 {
-    int A,B;
+    int A, B;
     scanf("%d %d", &A, &B);
-    const auto val = (A+B) %12;
-    printf(val == 0 ? "12\n" :"%d\n", val );
+    const auto val = (A + B) % 12;
+    printf(val == 0 ? "12\n" : "%d\n", val);
 }
