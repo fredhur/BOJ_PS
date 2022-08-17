9602a333f0cf8b4b475658395a04da80938c3d4a
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-18 00:59:17 +0900

    Solve 24085

---
+++ BOJ_2xxxx/boj_24085.py
@@ -0,0 +1,10 @@
+N = int(input())
+l = list(map(int, input().split()))
+D = dict()
+for i in l :
+    if i in D: D[i] += 1
+    else : D[i] = 1;
+
+a = sorted(D.items(), key=lambda x: (x[1], x[0]))
+
+print(a[0][0])
