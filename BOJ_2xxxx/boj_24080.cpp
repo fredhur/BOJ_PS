bc88b8c0e2ce83628cb2e13bd30cc122d0a76aa3
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-18 00:17:31 +0900

    Solve 24080

---
+++ BOJ_2xxxx/boj_24080.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+int N, cnt[26];
+int main()
+{
+ 
+    scanf("%d", &N);
+    for(int i=0; i<N; i++)
+    {
+        char ch;
+        scanf(" %c", &ch);
+        cnt[ch-'A'] =1;
+    }
+    int sum = 0;
+    for(int i=0; i<26; i++)
+    {
+        sum += cnt[i];
+    }
+    puts(sum >= 3 ? "Yes":"No");
+}
