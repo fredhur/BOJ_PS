b7ccd40bbf6c78ca902eb2ab644719187c866e8b
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-20 02:00:06 +0900

    Solve 18416

---
+++ BOJ_1xxxx/boj_18416.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+int N;
+int arr[105];
+
+
+bool check(int idx, int len)
+{
+    for(int i= idx; i< idx+len -1  ; i++)
+    {
+        if(arr[i] > arr[i+1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    scanf("%d", &N);
+    for(int i=1; i<=N; i++)
+    {
+        scanf(" %d", &arr[i]);
+    }
+
+    for(int s = N; s>=1; s--)
+    {
+        for(int i=1; i<= N-s+1; i++ )
+        {
+            if(check(i, s))
+            {
+                printf("%d\n", s);
+                return 0;
+            }
+        }
+    }
+    printf("1\n");
+    return 0;
+}
