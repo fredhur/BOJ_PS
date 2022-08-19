f09c873e53fe9170eb0b5b9a562658756995d289
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-20 01:59:19 +0900

    Solve 18413

---
+++ BOJ_1xxxx/boj_18413.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+int N,M;
+int saveN[105];
+int main()
+{
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; i++)
+    {
+        int n;
+        scanf(" %d", &n);
+        saveN[n]++;
+        
+    }
+
+    int max = -1;
+    for(int i=1; i<=M; i++)
+    {
+        max = (max < saveN[i]) ? saveN[i] : max;
+    }
+    printf("%d\n", max);
+}
