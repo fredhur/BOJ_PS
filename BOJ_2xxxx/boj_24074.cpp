41436fc437245797584f43e9e1156915579cd285
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-23 23:57:18 +0900

    Solve 24074

---
+++ BOJ_2xxxx/boj_24074.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+int N;
+int arr[105];
+
+int CalcMaxNumIdx(int (&arr)[105])
+{
+
+    int ret = arr[0];
+    int idx = 0;
+    for (int i = 0; i < N; i++)
+    {
+        if (ret < arr[i])
+        {
+            ret = arr[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+int main()
+{
+
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf(" %d", &arr[i]);
+    }
+
+    int idx = CalcMaxNumIdx(arr);
+
+    auto backSum = 0;
+    auto frontSum = 0;
+    for (int i = 0; i < idx; i++)
+    {
+        frontSum += arr[i];
+    }
+
+    for (int i = idx + 1; i < N; i++)
+    {
+        backSum += arr[i];
+    }
+    printf("%d\n%d\n", frontSum, backSum);
+
+    return 0;
+}
