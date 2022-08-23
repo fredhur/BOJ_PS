3f4c64cf3a77833a3c186a49231e4d6905142288
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-24 00:23:02 +0900

    Solve 24076

---
+++ BOJ_2xxxx/boj_24076.cpp
@@ -0,0 +1,78 @@
+#include <cstdio>
+class State 
+{
+public:
+    State()
+    {
+        state = 's';
+        changeCnt = 0;
+    }
+    ~State() = default;
+    int GetCnt() const 
+    {
+        return changeCnt;
+    }
+    void Calc(char ch)
+    {
+        if(state == 's') // start state
+        {
+            if(ch == 'I')
+            {
+                state = 'I';
+                return;
+            }
+            else 
+            {
+                state= 'I';
+                changeCnt++;
+                return;
+            }
+        }
+        else if(state == 'I')
+        {
+            if(ch == 'O')
+            {
+                state = 'O';
+                return;
+            }
+            else 
+            {
+                state = 'O';
+                changeCnt++;
+                return;
+            }
+        }
+        else if(state == 'O')
+        {
+            if(ch=='I')
+            {
+                state = 'I';
+                return;
+            }
+            else 
+            {
+                state = 'I';
+                changeCnt++;
+                return;
+            }
+        }
+    }
+private:
+
+    char state;
+    int changeCnt ;
+}state;
+int main()
+{
+    
+    int N;
+    scanf("%d", &N);
+    for(int i=0; i<N; i++)
+    {
+        char ch;
+        scanf(" %1c", &ch);
+        state.Calc(ch);
+    }
+    printf("%d\n", state.GetCnt());
+    return 0;
+}
