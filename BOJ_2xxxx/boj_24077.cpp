9d2d6fd2424991172d1f91b363df76086c28f2b2
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-24 00:27:19 +0900

    Solve 24077

---
+++ BOJ_2xxxx/boj_24077.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+int A[103];
+int B[103];
+int ans;
+int main()
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; i++)
+    {
+        scanf(" %d", &A[i]);
+    }
+    for(int j=0; j<M; j++)
+    {
+        scanf(" %d", &B[j]);
+    }
+    for(int i=0; i<N; i++)
+    {
+        for(int j=0; j<M; j++)
+        {
+            if(A[i] <= B[j])
+            {
+                ans++;
+            }
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
+}
