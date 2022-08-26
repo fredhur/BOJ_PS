bb32795b4e76390fd21a4c42e2c39904cb542917
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-27 01:52:01 +0900

    Solve 16785

---
+++ BOJ_1xxxx/boj_16785.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+#include <cmath>
+int main()
+{
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    const int total = 7*A + B;
+
+    const int ret = C/total*7 + std::min((int)ceil((double)(C % total) / (double)A), 7);
+    printf("%d\n", ret);
+    return 0;
+}
