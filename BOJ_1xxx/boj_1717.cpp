f5c88093af2faf1a35c6513fb9212543605658f1
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-27 01:50:44 +0900

    Solve 1717

---
+++ BOJ_1xxx/boj_1717.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+int n, m;
+int parent[1'000'007];
+void init()
+{
+    for (int i = 0; i <= n; i++)
+    {
+        parent[i] = i;
+    }
+}
+int findRoot(int x)
+{
+    if (x == parent[x])
+    {
+        return x;
+    }
+
+    return parent[x] = findRoot(parent[x]);
+}
+void Union(int x, int y)
+{
+    const auto rootX = findRoot(x);
+    const auto rootY = findRoot(y);
+
+    parent[rootY] = rootX;
+
+}
+int main()
+{
+    scanf("%d %d", &n, &m);
+    init();
+    for (int i = 0; i < m; i++)
+    {
+        int cmd;
+        int a, b;
+        scanf(" %d %d %d", &cmd, &a, &b);
+        if (cmd == 0)
+        {
+            Union(a, b);
+        }
+        else
+        {
+            puts(findRoot(a) == findRoot(b) ? "YES" : "NO");
+        }
+    }
+}
