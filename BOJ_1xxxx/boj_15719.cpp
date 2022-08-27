9af8d2bc59eb68489c2eb2069e9624bcbd89553b
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-08-27 18:53:48 +0900

    Solve 15719

---
+++ BOJ_1xxxx/boj_15719.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+using ll = long long;
+int main()
+{
+    ll N;
+    scanf("%lld", &N);
+    ll sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        ll n;
+        scanf(" %lld", &n);
+        sum += n;
+    }
+    printf("%lld\n", sum - ((N) * (N - 1) / 2 ));
+    return 0;
+}
