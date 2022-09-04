b7e329cb24d712a8607a32ac9563f0da431610e3
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-09-05 00:37:42 +0900

    Solve 4779

---
+++ BOJ_4xxx/boj_4779.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+int N;
+int threeSquare(int a, int N)
+{
+    if (N == 0)
+        return 1;
+
+    if (N == 1)
+        return a;
+
+    auto tmp = threeSquare(a, N / 2);
+    if (N % 2 == 0)
+        return tmp * tmp;
+    else
+        return a * tmp * tmp;
+}
+void KantoreString(int numIdx, int len)
+{
+
+    if (len == 1)
+    {
+        printf("-");
+        return;
+    }
+    KantoreString(numIdx, len / 3);
+
+    for (int i = 0; i < len / 3; i++) printf(" ");
+    
+    KantoreString(numIdx + 2 * len / 3, len / 3);
+}
+int main()
+{
+    while (scanf("%d", &N) != EOF)
+    {
+        KantoreString(0, threeSquare(3, N));
+        puts("");
+    }
+    return 0;
+}
