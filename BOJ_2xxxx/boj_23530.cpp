5683f0765df8e799424ae664b4a9bba9981d616f
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-09-04 23:04:25 +0900

    Solve 23530

---
+++ BOJ_2xxxx/boj_23530.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+int solve(int sum)
+{
+    for(int i=1; i<=50; i++)
+    {
+        if(i != sum)
+        {
+            return i;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    int tc;
+    scanf("%d", &tc);
+    while(tc--)
+    {
+        int a,b;
+        scanf(" %d %d", &a, &b);
+        printf("%d\n", solve(a+b));
+    }
+    return 0;
+}
