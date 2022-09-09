93ffe23c5df5abad9c1780ed90c2f308074fd349
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-09-10 00:11:06 +0900

    Solve 11582

---
+++ BOJ_1xxxx/boj_11582.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+using namespace std;
+vector<int> v;
+vector<int> ret;
+int main()
+{
+    int N,K;
+    scanf("%d", &N);
+    for(int i=0; i<N; i++)
+    {
+        int num;
+        scanf(" %d", &num);
+        v.push_back(num);
+    }
+    scanf(" %d", &K);
+    const int s = N/K;
+    for(auto &i: v)
+    {
+        ret.push_back(i);
+        if(ret.size() == s)
+        {
+            sort(ret.begin(), ret.end());
+            for(auto &i : ret)
+            {
+                printf("%d ", i);
+            }
+            ret.clear();
+        }
+    }
+    puts("");
+    return 0;
+}
