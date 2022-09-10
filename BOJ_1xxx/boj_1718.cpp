982150a5113a0a56c53c223dff0a8e3a0eda34f6
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-09-10 14:53:46 +0900

    Solve 1718

---
+++ BOJ_1xxx/boj_1718.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+char str[30007];
+char key[30007];
+char convert(char ch, char key)
+{
+  
+    int delta = ch -key -1;
+   
+    if(delta < 0)
+    {
+        return 'z' + delta + 1 ;
+    }
+    else 
+    {
+        return delta + 'a';
+    }
+   
+}
+int strlength(char str[])
+{
+    int i=0;
+    for(i=0; str[i]; i++);
+    return i;
+}
+int main()
+{
+    scanf("%[^\n]", str);
+    scanf(" %s", key);
+    const int keylen = strlength(key);
+    int j=0;
+    for(int i=0; str[i]; i++)
+    {   
+        if(str[i] == ' ')
+        {
+            j++;
+            j = j%(keylen);
+            printf(" ");
+            continue;
+        }
+
+        printf("%c", convert(str[i], key[j++]));
+        j = j%keylen;
+    }
+    puts("");
+    return 0;
+}
