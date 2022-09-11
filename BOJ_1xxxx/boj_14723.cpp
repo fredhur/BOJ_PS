5fa0be54e085dd08cdcf41397e6e3be1281f9305
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-09-12 01:37:14 +0900

    Solve 14723

---
+++ BOJ_1xxxx/boj_14723.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+int CalcPrev(int N)
+{
+    int sum = 0;
+    for (int i = 1;; i++)
+    {
+        sum += i;
+        if (sum >= N)
+        {
+            return i - 1;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    const int n = CalcPrev(N);
+    if (n == N)
+    {
+        printf("%d %d\n", 1, n);
+    }
+    else
+    {
+        const int r = N - (n * (n + 1) / 2);
+
+        printf("%d %d\n", n + 2 - r, r);
+    }
+    return 0;
+}
