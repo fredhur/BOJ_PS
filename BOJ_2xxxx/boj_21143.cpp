033841edecbf61a7750b1e4e5aea7b3c1d5a766e
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-10-16 22:21:44 +0900

    Solve 21143

---
+++ BOJ_2xxxx/boj_21143.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+int lut[26];
+int main()
+{
+    char str[55];
+    scanf("%s",str);
+    bool flag = false;
+    for(int i=0; str[i]; i++)
+    {
+        char ch = str[i];
+        lut[ch-'a']++;
+        if(lut[ch-'a'] == 2)
+        {
+            flag = true;
+            break;
+        }
+    }
+    puts(flag == true ? "0" : "1" );
+
+    return 0;
+}
