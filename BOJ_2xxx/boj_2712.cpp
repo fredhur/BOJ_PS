3faf4474b4e1aa061f1f9cc5326d2394f211a421
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-10-23 23:34:44 +0900

    aligning in 2712

--- BOJ_2xxx/boj_2712.cpp
+++ BOJ_2xxx/boj_2712.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 int tc;
-char * s[]={"lb", "l", "g", "kg"};
-const int M(const char *s){
+char *s[] = {"lb", "l", "g", "kg"};
+const int M(const char *s)
+{
     const auto kg = 'k' + 'g';
     const auto g = 'g';
     const auto l = 'l';
     const auto lb = 'l' + 'b';
-    int a = s[1]==0?s[0]:s[0]+s[1];
-    switch (a){
+    int a = s[1] == 0 ? s[0] : s[0] + s[1];
+    switch (a)
+    {
     case kg:
         return 0;
     case g:
@@ -20,17 +22,20 @@ const int M(const char *s){
         return 4;
     }
 }
-double C(char *s){
+double C(char *s)
+{
     static double c[4] = {2.2046, 3.7854, 0.2642, 0.4536};
     return c[M(s)];
 }
-int main(){
-    scanf("%d",&tc);
-    while (tc--){
+int main()
+{
+    scanf("%d", &tc);
+    while (tc--)
+    {
         double t;
         char u[2];
         scanf(" %lf %s", &t, u);
-        printf("%.4lf %s\n", t * C(u),s[M(u)]);
+        printf("%.4lf %s\n", t * C(u), s[M(u)]);
     }
     return 0;
 }
