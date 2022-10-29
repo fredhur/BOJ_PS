305498749a97492ed279ebbba23a3671e4b57ffa
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-10-29 18:30:46 +0900

    Solve 7120

---
+++ BOJ_7xxx/boj_7120.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+char str[252];
+auto mstrlen(char *s)
+{
+    int i;
+    for (i = 0; s[i]; i++);
+    return i;
+}
+int main()
+{
+    scanf("%s", str);
+    printf("%c", str[0]);
+
+    if (mstrlen(str) == 1)
+    {
+        puts("");
+        return 0;
+    }
+    else
+    {
+        for (int i = 1; str[i]; i++)
+        {
+            if (str[i] != str[i - 1])
+            {
+                printf("%c", str[i]);
+            }
+        }
+    }
+    puts("");
+    return 0;
+}
