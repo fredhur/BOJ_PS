08cf00f0b483c05a9ddcb52be6dcd85de73e92c0
HurJoonHyung <fredhur@HurJoonHyungui-MacBookAir.local>
2022-11-12 22:40:23 +0900

    Solve 25631

---
+++ BOJ_2xxxx/boj_25631.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+priority_queue<int> pq;
+int N;
+int arr[1004];
+int idx;
+int main()
+{
+    scanf("%d", &N);
+    for(int i=0; i<N; i++)
+    {
+        int n;
+        scanf(" %d", &n);
+        pq.push(n);
+    }
+   
+    
+    while(!pq.empty())
+    {
+        int top = pq.top();
+        pq.pop();
+
+        bool flag = false;
+        for(int i=0; i<=idx; i++)
+        {
+            if(arr[i] > top)
+            {
+                arr[i] = top;
+                flag = true;
+                break;
+            }
+        }
+        if(flag == false)
+        {
+            idx++;
+            arr[idx] = top;
+        }
+    }
+    printf("%d\n", idx);
+    return 0;
+}
